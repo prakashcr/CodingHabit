4ca2e515182acc3c0344c4d0a9358e21dcb79d9f
Prakash <prakash8296@gmail.com>
2021-03-17 21:42:51 +0530

    Tricks

---
+++ Codes/src/Tricks/Sysoutwthoutsemicolon.java
@@ -0,0 +1,12 @@
+package Tricks;
+
+public class Sysoutwthoutsemicolon {
+    public static void main(String[] args) {
+        for (int i = 0; i<1; System.out.println("Hello World")) {
+            i++;
+        }
+        if((System.out.printf("Hello World")==null)){
+
+        }
+    }
+}
---
+++ Codes/src/Tricks/print1to100.java
@@ -0,0 +1,13 @@
+package Tricks;
+
+public class print1to100 {
+    public static void main(String[] arg){
+        int one = 'a'/'a';
+        System.out.println("value is " +one);
+        String s = "..........";
+        for (int i=one;i<=(s.length()*s.length());i++){
+            System.out.println(i);
+
+        }
+    }
+}
